2ad5c6d476a25d2333aee39cb7bf5e2c7eb9d031
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-03-08 17:46:12 +0900

    マイグレファイルの削除

--- db/migrate/20200204091912_user_movies.rb
+++
@@ -1,5 +0,0 @@
-class UserMovies < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :user_movies
-  end
-end
--- db/migrate/20200205030728_feeling_movies.rb
+++
@@ -1,5 +0,0 @@
-class FeelingMovies < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :feeling_movies
-  end
-end
