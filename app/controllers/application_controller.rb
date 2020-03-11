9a56fb8c5fb8daa43b0c3d423405b17c91afbacc
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-03-11 20:12:53 +0900

    BASIC認証を環境変数に置き換える

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -12,9 +12,17 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def production?
+    Rails.env.production?
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+end
+
     end
   end
 
